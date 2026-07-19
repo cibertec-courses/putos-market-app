f7f82a51eeab5ecd776099331065ab39408fac45
Christiam Mendoza <ch.alb.rz@hotmail.com>
2026-07-19 10:27:12 -0500

    Initial commit

---
+++ src/main/java/edu/pe/cibertec/puntosmarket/PuntosmarketApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.puntosmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntosmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntosmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/puntosmarket/PuntosmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.puntosmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntosmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
